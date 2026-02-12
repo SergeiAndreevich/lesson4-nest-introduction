32e2c7818a3201ae6faf9cc88c95c1683b4268d4
SergeiAndreevich <sergeytroshin00@gmail.com>
2026-02-12 16:24:07 +0300

    completed guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,53 +1,55 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, HttpCode} from '@nestjs/common';
+import {Controller, Get, Post, Body, HttpCode, UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import {LoginInputDto} from "./dto/login-input.dto";
 import {EmailInputDto} from "./dto/email-input-dto";
 import {NewPasswordInputDto} from "./dto/new-password-input.dto";
 import {CodeInputDto} from "./dto/code-input.dto";
+import {BearerGuard} from "../guards/bearer.guard";
+import {UserId} from "../customDecorators/userId.decorator";
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/login')
+  @Post('login')
   loginUser(@Body() loginInputDto: LoginInputDto) {
     return this.authService.loginUser(loginInputDto);
   }
 
-  @Post('/password-recovery')
+  @Post('password-recovery')
   @HttpCode(204)
   recoveryUserPassword(@Body() emailInputDto: EmailInputDto) {
     return this.authService.recoveryUserPassword(emailInputDto);
   }
 
-  @Post('/new-password')
+  @Post('new-password')
   @HttpCode(204)
   setNewPassword(@Body() newPasswordInputDto: NewPasswordInputDto){
     return this.authService.setNewPassword(newPasswordInputDto);
   }
 
-  @Post('/registration')
+  @Post('registration')
   @HttpCode(204)
   registration(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.registration(createAuthDto);
   }
 
-  @Post('/registration-confirmation')
+  @Post('registration-confirmation')
   @HttpCode(204)
   registrationConfirmation(@Body() codeInputDto: CodeInputDto) {
     return this.authService.registrationConfirmation(codeInputDto);
   }
 
-  @Post('/registration-email-resending')
+  @Post('registration-email-resending')
   @HttpCode(204)
   registrationEmailResending(@Body() emailInputDto: EmailInputDto) {
     return this.authService.registrationEmailResending(emailInputDto);
   }
 
-  @Get('/me')
-  findMe() {
-    return this.authService.findMe();
+  @Get('me')
+  @UseGuards(BearerGuard)
+  async findMe(@UserId()userId:string ) {
+    return this.authService.findMe(userId)
   }
 }
--- src/guards/bearer.guard.ts
+++ src/guards/bearer.guard.ts
@@ -1,11 +1,41 @@
-import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
+import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "@nestjs/common";
 import {Observable} from "rxjs";
+import {JwtService} from "@nestjs/jwt";
 
 @Injectable()
 export class BearerGuard implements CanActivate {
+    constructor(private readonly jwtService: JwtService) {}
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest();
-        //request.headers.authorization
-        return true
+        //проверяем, пришел ли токен
+        const userAuth = request.headers.authorization;
+        if(!userAuth) {
+            throw new UnauthorizedException()
+        }
+
+        //если пришло что-то в headers, надо это оттуда достать
+        const [authType, token] = userAuth.split(' ');
+        //проверяем, какая это авторизация. Нам нужна именно bearer, другая идет лесом
+        //если не токен-авторизация, то не выдадим доступ
+        if(authType !== 'Bearer') {
+            throw new UnauthorizedException()
+        }
+        //если не извлекся токен
+        if (!token){
+            throw new UnauthorizedException()
+        }
+
+
+        try {
+            const payload = this.jwtService.verify(token);
+            if (!payload || typeof payload !== 'object' || !('userId' in payload)) {
+                throw new UnauthorizedException();
+            }
+            request.user = { userId: payload.userId };
+            return true
+        } catch (e) {
+            console.error(e);
+            throw new UnauthorizedException()
+        }
     }
 }
